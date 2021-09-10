c72f738e8712a70225a1eab20f6b93f4fcd8b4f2
John Matula <john@johnmatu.la>
2021-09-10 14:21:00 -0400

    Use actions for cart-adding methods

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ export default new Vuex.Store({
         id: 0,
         title: "Cranberry Nut Trail Mix (20 oz.)",
         image: "https://picsum.photos/200/200",
-        price: "54.52",
+        price: 54.52,
         isBestSeller: true,
         availableQuantity: 1,
         cartQuantity: 0
@@ -19,7 +19,7 @@ export default new Vuex.Store({
         id: 1,
         title: "iPhone 13?",
         image: "https://picsum.photos/200/200",
-        price: "999",
+        price: 999,
         isBestSeller: false,
         availableQuantity: 1,
         cartQuantity: 0
@@ -28,7 +28,7 @@ export default new Vuex.Store({
         id: 2,
         title: "RuPaul Sashay Shante Enamel Pin",
         image: "https://hottopic.scene7.com/is/image/HotTopic/11419583_hi?$productMainDesktopRetina$",
-        price: "5.52",
+        price: 5.52,
         isBestSeller: false,
         availableQuantity: 1,
         cartQuantity: 0
@@ -36,14 +36,10 @@ export default new Vuex.Store({
     ]
   },
   mutations: {
-    addToCart(state, product, amount = 1) {
-      state.products[product].availableQuantity -= amount
-      state.products[product].cartQuantity += amount
+    adjustQuantities(state, payload) {
+      state.products[payload.id].availableQuantity += payload.availableAmount
+      state.products[payload.id].cartQuantity += payload.cartAmount
     },
-    removeFromCart(state, product, amount = 1) {
-      state.products[product].cartQuantity -= amount
-      state.products[product].availableQuantity += amount
-    }
   },
   getters: {
     availableProducts: (state) => {
@@ -54,7 +50,19 @@ export default new Vuex.Store({
     }
   },
   actions: {
-  },
-  modules: {
-  },
+    addToCart({ commit }, payload) {
+      commit('adjustQuantities', {
+        id: payload.id,
+        availableAmount: payload.amount * -1,
+        cartAmount: payload.amount
+      })
+    },
+    removeFromCart({ commit }, payload) {
+      commit('adjustQuantities', {
+        id: payload.id,
+        availableAmount: payload.amount,
+        cartAmount: payload.amount * -1
+      })
+    }
+  }
 })
