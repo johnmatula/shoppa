04a49caca4f3febee928425d41bc640f6a3012f6
John Matula <john@johnmatu.la>
2021-09-14 10:16:34 -0400

    Add typography partial to sass-loader

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
         data: `
           @import "@/styles/_mixins.scss";
           @import "@/styles/_variables.scss";
+          @import "@/styles/_typography.scss";
         `
       }
     }
